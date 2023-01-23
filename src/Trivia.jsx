673a19831976477f5063a116ff4a903ba805600d
gabycode <86427446+gabycode@users.noreply.github.com>
2023-01-23 16:31:27 -0400

    Update Trivia.jsx

--- src/Trivia.jsx
+++ src/Trivia.jsx
@@ -1,8 +1,19 @@
-export default function Trivia(props) {
+import './Trivia.css'
+
+export default function Trivia({question, answers}) {
+    
+    const answersJSX = answers.map((answer, index) => (
+    <div key={index} className="trivia-answers">
+      {answer}
+    </div>
+  ))
+  console.log(answersJSX)
     return(
         <div>
-            <h1>{props.question}</h1>
-            <p>{props.answers}</p>
+            <h1 className="trivia-question">{question}</h1>
+            <div className="answers">
+                {answersJSX}
+            </div>
         </div>
     )
 }
