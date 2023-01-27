c6e8fbbb060c3a48e665bf9410ac0c7ce6660021
gabycode <86427446+gabycode@users.noreply.github.com>
2023-01-27 16:32:51 -0400

    Create Button.jsx

---
+++ components/Button.jsx
@@ -0,0 +1,9 @@
+import "../src/Trivia.css";
+
+export default function CheckAnswersBtn() {
+  return (
+    <div className="btn-container">
+      <button className="check-answers-btn">Check answers</button>
+    </div>
+  );
+}
