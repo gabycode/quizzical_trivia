434ed3c3f032957511b97e8399702af88f8f0a82
gabycode <86427446+gabycode@users.noreply.github.com>
2023-02-02 18:56:01 -0400

    Update Answer.jsx

--- components/Answer.jsx
+++ components/Answer.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import "../src/Trivia.css";
+import { decode } from "html-entities";
 
-const Answer = ({ answer, setSelectedAnswer, selectedAnswer }) => {
+const Answer = ({
+  answer,
+  setSelectedAnswer,
+  selectedAnswer,
+  correctAnswer,
+  onCheckAnswers,
+}) => {
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  function checkAnswers() {
+    if (selectedAnswer === correctAnswer) {
+      console.log("correct");
+    } else {
+      console.log("incorrect");
+    }
+  }
+
+  checkAnswers();
+
   return (
     <div
       className={`trivia-answers ${
@@ -8,10 +28,10 @@ const Answer = ({ answer, setSelectedAnswer, selectedAnswer }) => {
       }`}
       onClick={() => {
         setSelectedAnswer(answer);
-        // console.log(answer);
+        checkAnswers;
       }}
     >
-      {answer}
+      {decode(answer)}
     </div>
   );
 };
