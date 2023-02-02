2006dc61f2a5f26244658ca9939d9963ba9bb2bf
gabycode <86427446+gabycode@users.noreply.github.com>
2023-02-02 18:56:04 -0400

    Update Button.jsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,29 +1,14 @@
 import { useState } from "react";
 import "../src/Trivia.css";
 
-export default function CheckAnswersBtn({ triviaJSX }) {
-  const [correctAnswer, setCorrectAnswer] = useState("Cheetah");
-
-  // console.log(triviaArr);
-
-  function handleClick() {
+export default function CheckAnswersBtn({ triviaData }) {
+  const checkAnswers = () => {
     console.log("checking answers");
-    const triviaArr = triviaJSX.map((t) => {
-      if (correctAnswer === t.props.correctAnswer) {
-        console.log("correct answer");
-      } else if (correctAnswer !== t.props.correctAnswer) {
-        console.log("incorrect answer");
-      } else {
-        console.log("not answered");
-      }
-    });
-  }
+  };
 
   return (
     <div className="btn-container">
-      <button className="check-answers-btn" onClick={handleClick}>
-        Check answers
-      </button>
+      <button className="check-answers-btn">Check answers</button>
     </div>
   );
 }
